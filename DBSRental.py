5dd2ed80aa30e9f3514d7c2760a34f7c2ad5aaf6
Eilish Murphy <eilish76@gmail.com>
2016-06-25 15:22:45 +0100

    DBSRental.py: Quantity function added, return from functions changed to facilitate testing. Test_DBSRental.py created and uploaded

--- DBSRental.py
+++ DBSRental.py
@@ -28,31 +28,27 @@ class Dealership(object):
 
     # function to return current stock level
     def stock_count(self):
-        print '\n\tCurrent Stock:'
-        print '\t\t%d Petrol Cars' %len(self.petrol_cars)
-        print '\t\t%d Diesel Cars' %len(self.diesel_cars)
-        print '\t\t%d Electric Cars' %len(self.electric_cars)
-        print '\t\t%d Hybrid Cars' %len(self.hybrid_cars)
+        return '\n\tCurrent Stock:\n\t\t%d Petrol Cars\n\t\t%d Diesel Cars\n\t\t%d Electric Cars\n\t\t%d Hybrid Cars\n' % (len(self.petrol_cars), len(self.diesel_cars), len(self.electric_cars), len(self.hybrid_cars))
         
         
     # function to process rental 
     def rent(self, car_list, required):
         if len(car_list) < required:
-            print '\nThere are not enough cars in stock to fulfil rental\n'
-            self.stock_count()
+            return '\nThere are not enough cars in stock to fulfil rental\n' 
+            
         else:
             count = 0
             while count < required:
                 car_list.pop()
                 count = count + 1
                 
-            print 'You have sucessfully rented %d cars\n' %required
+            return 'You have successfully rented %d cars\n' %required
         
     # function to return rented vehicle
     def restock(self, car_list, returned, Car):
         for i in range(returned):
             car_list.append(Car())
-        print 'You have sucessfully returned %d cars' %returned    
+        return 'You have successfully returned %d cars' %returned    
        
         
      # function to request car type from user
@@ -69,7 +65,17 @@ class Dealership(object):
                 print "Selection must be 'p', 'd', 'e', 'h' or 'n', Try again"
                 continue
             
-        
+    # function to get number of cars from user
+    def get_quantity(self):
+        while True:    
+            quantity = raw_input('How many cars?\n> ')
+            try:
+                quantity = int(quantity)
+                return quantity
+            except:
+                print 'integer input only please'
+                continue
+    
         
 if __name__ == '__main__': 
     
@@ -79,6 +85,7 @@ if __name__ == '__main__':
     DBSRental.create_current_stock()
     
     print 'Welcome to DBS Car Rental\n'
+    print DBSRental.stock_count()
     
     #get user to select rent/return mode
     while True:
@@ -94,23 +101,19 @@ if __name__ == '__main__':
                 
                 car_type = DBSRental.select_car_type()
                 
-                required = raw_input('How many cars would you like?\n> ')
-                try:
-                    required = int(required)
-                except:
-                    continue
-                
+                required = DBSRental.get_quantity()
+                                
                 if car_type == 'p':
-                    DBSRental.rent(DBSRental.petrol_cars, required)
+                    print DBSRental.rent(DBSRental.petrol_cars, required)
                     break
                 if car_type == 'd':
-                    DBSRental.rent(DBSRental.diesel_cars, required)
+                    print DBSRental.rent(DBSRental.diesel_cars, required)
                     break
                 if car_type == 'e':
-                    DBSRental.rent(DBSRental.electric_cars, required)
+                    print DBSRental.rent(DBSRental.electric_cars, required)
                     break
                 if car_type == 'h':
-                    DBSRental.rent(DBSRental.hybrid_cars, required)
+                    print DBSRental.rent(DBSRental.hybrid_cars, required)
                     break
                    
         
@@ -121,35 +124,31 @@ if __name__ == '__main__':
                 
                 car_type = DBSRental.select_car_type()
                 
-                returned = raw_input('How many cars are you returning?\n> ')
-                try:
-                    returned = int(returned)
-                except:
-                    continue
+                returned = DBSRental.get_quantity()
                 
                 if car_type == 'p':
                     if (len(DBSRental.petrol_cars) + returned) > 24:
                         print 'Please check type and quantity: exceeds max stock'
                         continue
-                    DBSRental.restock(DBSRental.petrol_cars, returned, PetrolCar)
+                    print DBSRental.restock(DBSRental.petrol_cars, returned, PetrolCar)
                     break
                 if car_type == 'd':
                     if (len(DBSRental.diesel_cars) + returned) > 12:
                         print 'Please check type and quantity: exceeds max stock'
                         continue
-                    DBSRental.restock(DBSRental.diesel_cars, returned, DieselCar)
+                    print DBSRental.restock(DBSRental.diesel_cars, returned, DieselCar)
                     break
                 if car_type == 'e':
                     if (len(DBSRental.electric_cars) + returned) > 4:
                         print 'Please check type and quantity: exceeds max stock'
                         continue
-                    DBSRental.restock(DBSRental.electric_cars, returned, ElectricCar)
+                    print DBSRental.restock(DBSRental.electric_cars, returned, ElectricCar)
                     break
                 if car_type == 'h':
                     if (len(DBSRental.hybrid_cars) + returned) > 4:
                         print 'Please check type and quantity: exceeds max stock'
                         continue
-                    DBSRental.restock(DBSRental.hybrid_cars, returned, HybridCar)
+                    print DBSRental.restock(DBSRental.hybrid_cars, returned, HybridCar)
                     break
                 
                     
---
+++ Test_DBSRental.py
@@ -0,0 +1,110 @@
+#### Name: Eilish Murphy
+#### Number: 10190433
+#### CA2 - DBS Car Rental Program
+
+###  Test_DBSRental.py: Test functionality of Dealership Object
+
+import unittest
+
+from DBSRental import Dealership
+from DBSCars import Car, PetrolCar, DieselCar, ElectricCar, HybridCar
+
+## test the Dealership class functionality
+class TestDealership(unittest.TestCase):
+
+    def setUp(self):
+        self.dealer = Dealership()
+        self.dealer.create_current_stock()
+        
+        
+    # test create current stock functionality
+    # should create 24 petrol cars, 12 diesel cars, 4 electric cars and 4 hybrid cars
+    def test_create_current_stock(self):
+        self.assertEqual(24, len(self.dealer.petrol_cars))
+        self.assertEqual(12, len(self.dealer.diesel_cars))
+        self.assertEqual(4, len(self.dealer.electric_cars))
+        self.assertEqual(4, len(self.dealer.hybrid_cars))
+        
+    # test stock count function 
+    # return should be '\n\tCurrent Stock:\n\t\t%d Petrol Cars\n\t\t%d Diesel Cars\n\t\t%d Electric Cars\n\t\t%d Hybrid Cars\n' % (len(self.petrol_cars), len(self.diesel_cars), len(self.electric_cars), len(self.hybrid_cars))
+    def test_stock_count(self):
+        result = '\n\tCurrent Stock:\n\t\t%d Petrol Cars\n\t\t%d Diesel Cars\n\t\t%d Electric Cars\n\t\t%d Hybrid Cars\n' % (len(self.dealer.petrol_cars), len(self.dealer.diesel_cars), len(self.dealer.electric_cars), len(self.dealer.hybrid_cars))
+        self.assertEqual(result, self.dealer.stock_count())
+    
+        
+    # test rent function when car stock exceeds vehicles required
+    # rent 2 cars from each type and check remaining stock 
+    # remaining should be 22 petrol, 10 diesel, 2 electric, and 2 hybrid
+    # return should equal 'You have successfully rented %d cars\n' %required
+    def test_rent_cars_available(self):
+        required = 2
+        result = 'You have successfully rented %d cars\n' %required
+        
+        self.dealer.rent(self.dealer.petrol_cars, required)
+        self.assertEqual(22, len(self.dealer.petrol_cars))
+        self.assertEqual(result, self.dealer.rent(self.dealer.petrol_cars, required))
+        
+        self.dealer.rent(self.dealer.diesel_cars, required)
+        self.assertEqual(10, len(self.dealer.diesel_cars))
+        self.assertEqual(result, self.dealer.rent(self.dealer.diesel_cars, required))
+                
+        self.dealer.rent(self.dealer.electric_cars, required)
+        self.assertEqual(2, len(self.dealer.electric_cars))
+        self.assertEqual(result, self.dealer.rent(self.dealer.electric_cars, required))
+               
+        self.dealer.rent(self.dealer.hybrid_cars, required)
+        self.assertEqual(2, len(self.dealer.hybrid_cars))
+        self.assertEqual(result, self.dealer.rent(self.dealer.hybrid_cars, required))
+    
+    # test rent function when car stock is less than vehicles required
+    # rent 50 cars from each type and check return = '\nThere are not enough cars in stock to fulfil rental\n'
+    def test_rent_cars_not_available(self):
+        result = '\nThere are not enough cars in stock to fulfil rental\n'
+        self.assertEqual(result, self.dealer.rent(self.dealer.petrol_cars, 50))
+        self.assertEqual(result, self.dealer.rent(self.dealer.diesel_cars, 50))
+        self.assertEqual(result, self.dealer.rent(self.dealer.electric_cars, 50))
+        self.assertEqual(result, self.dealer.rent(self.dealer.hybrid_cars, 50))
+        
+    # test restock function
+    # return 2 cars to each type and check stock balance
+    # len(list) after return should be petrol 26, diesel 14, electric 6, and hybrid 6
+    # return should be 'You have successfully returned %d cars' %returned
+    def test_restock(self):
+        returned = 2 
+        result = 'You have successfully returned %d cars' %returned
+        
+        self.dealer.restock(self.dealer.petrol_cars, returned, PetrolCar)
+        self.assertEqual(26, len(self.dealer.petrol_cars))
+        self.assertEqual(result, self.dealer.restock(self.dealer.petrol_cars, 2, PetrolCar))
+        
+        self.dealer.restock(self.dealer.diesel_cars, returned, DieselCar)
+        self.assertEqual(14, len(self.dealer.diesel_cars))
+        self.assertEqual(result, self.dealer.restock(self.dealer.diesel_cars, 2, DieselCar))
+        
+        self.dealer.restock(self.dealer.electric_cars, returned, ElectricCar)
+        self.assertEqual(6, len(self.dealer.electric_cars))
+        self.assertEqual(result, self.dealer.restock(self.dealer.electric_cars, 2, ElectricCar))
+        
+        self.dealer.restock(self.dealer.hybrid_cars, returned, HybridCar)
+        self.assertEqual(6, len(self.dealer.hybrid_cars))
+        self.assertEqual(result, self.dealer.restock(self.dealer.hybrid_cars, 2, HybridCar))
+
+
+
+
+
+
+
+    
+    # import mock
+
+# def test_say_hello():
+     # with mock.patch('__builtin__.raw_input', return_value='dbw'):
+         # assert say_hello() == 'Hello dbw'
+
+     # with mock.patch('__builtin__.raw_input', side_effect=['dbw', 'uki']):
+         # assert say_hello() == 'Hello dbw'
+         # assert say_hello() == 'Hello uki'    
+      
+if __name__ == '__main__':
+    unittest.main()         
